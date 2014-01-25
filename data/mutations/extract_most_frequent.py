adb4ba510d6c832e6fd3791c3287850f4234aec3
Alexander Lex <alex@seas.harvard.edu>
2014-01-24 20:23:29 -0500

    gbm data and extraction script

---
+++ data/mutations/extract_most_frequent.py
@@ -0,0 +1,15 @@
+# loads the csv file into the dataframe, chooses the 100 columns 
+# with the highest sum-per column, and writes those to another csv
+# author: Alexander Lex
+
+import pandas as pd
+import numpy as np
+
+
+df = pd.read_csv('gbm_mutated.csv', index_col=0, delim_whitespace=True)
+
+sum_values = df.sum().order(ascending=False).head(100)
+
+
+top100 = df[sum_values.index]
+top100.to_csv('gbm_mutated_top100.csv');
