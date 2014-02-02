d246e81c3ebc2d0ec93451ebbb7eda7cf59a0ccf
Alexander Lex <alex@seas.harvard.edu>
2014-02-02 15:38:06 -0500

    cleaning up code

--- upset.js
+++ upset.js
@@ -84,12 +84,6 @@ function Set(setID, setName, combinedSets, setData) {
 
 }
 
-
-//d3.text("data/test/test.csv", "text/csv", dataLoad);
-//d3.text("data/fruit/fruit.csv", "text/csv", dataLoad);
-//d3.text("data/movies/movies_simplified.csv", "text/plain", dataLoad);
-
-//
 function dataLoad(data) {
     var dsv = d3.dsv(";", "text/plain");
     var rows = dsv.parseRows(data).map(function (row) {
@@ -206,21 +200,6 @@ function plot(plottingSets) {
     var matrixHeight = combinations * cellDistance;
     var h = matrixHeight + textHeight;
 
-// var yScale = d3.scale.ordinal().domain(d3.range(data.length))
-// .rangeRoundBands([ 0, h ], 0.3);
-
-//    var yScale = d3.scale.ordinal().rangeRoundBands([ paddingTop, h ], .1);
-//    var xScale = d3.scale.linear().domain([ 0, d3.max(data, function (d) {
-//        return d.academic;
-//    }) ]).range([ 0, w - textWidth - paddingSide ]);
-
-//    yScale.domain(data.map(function (d) {
-//        return d.schoolname;
-//    }));
-
-
-//    var xAxis = d3.svg.axis().scale(xScale).orient("top");
-
 
     d3.select("#vis").select("svg").remove();
     var svg = d3.select("#vis").append("svg").attr("width", w)
@@ -288,18 +267,9 @@ function plot(plottingSets) {
             height: cellSize})
         .style("fill", function (d) {
             return setScale(d);
-//            if (d == 0)
-//                return '#f0f0f0';
-//            else
-//                return '#636363';
         });
 
     // ------------------- set size bars --------------------
-
-//    svg.append('rect').attr({
-//        height: '100',
-//    width: 100
-//    })
 
 
     svg.append('rect')
@@ -337,12 +307,7 @@ function plot(plottingSets) {
             class: 'setSize',
             transform: "translate(" + xStartSetSizes + ", 0)", // " + (textHeight - 5) + ")"
             width: function (d) {
-//                console.log(d.setSize);
-//                console.log("Scale" + subSetSizeScale(5));
                 return subSetSizeScale(d.setSize);
-                //return d.setSize;
-//                console.log( sizeScale(d.setSize));
-//                return  sizeScale(d.setSize);
             },
             height: cellSize
         });
@@ -358,8 +323,6 @@ function plot(plottingSets) {
                     return a.nrCombinedSets - b.nrCombinedSets;
                 }
                 for (var i = 0; i < a.nrCombinedSets; i++) {
-//                    console.log(a.combinedSets);
-//                    console.log(b.combinedSets);
                     if (a.combinedSets[i] != b.combinedSets[i]) {
                         console.log("a " + a.combinedSets + " b " + b.combinedSets);
                         if (a.combinedSets[i] > 0) {
@@ -402,89 +365,4 @@ function plot(plottingSets) {
             }});
     }
 }
-
-//    svg.selectAll('.setSize').data(plottingSets).enter()
-//        .append('rect')
-//        .attr({
-//            class: 'setSize',
-//            x: function (d) {
-//                console.log(d);
-//                return (cellDistance) * d.combinedSets.length;
-//
-//            },
-//            y: function (d, i) {
-//                return cellDistance * i;
-//            },
-//            width: function (d) {
-//
-//                return  d.setSize;
-//            },
-//            height: cellSize
-//        });
-
-//// background color
-//    svg.append("rect").attr({
-//        id: "base",
-//        width: w,
-//        height: h,
-//        fill: "#eeeeee"
-//    });
-//
-//    svg.append("g").attr("class", "axis").attr("transform",
-//            "translate(" + textWidth + ", " + (paddingTop - 5) + ")").attr(
-//            "class", "x axis").call(xAxis);
-//
-//    var rectangles = svg.selectAll(".bar").data(plottingSets).enter().append("rect")
-//        .attr({
-//            class: "bar",
-//            width: function (d) {
-//
-//                return xScale(d.academic);
-//            },
-//            height: yScale.rangeBand,
-//            y: function (d) {
-//                return yScale(d.schoolname);
-//            },
-//            x: textWidth
-//        });
-//
-//    svg.selectAll(".barLabel").data(data).enter().append("text").text(
-//        function (d) {
-//            return d.schoolname.substring(0, truncateAfter);
-//        }).attr({
-//            class: "barLabel",
-//            y: function (d) {
-//                return yScale(d.schoolname) + 15;
-//            },
-//            x: paddingSide,
-//            height: yScale.rangeBand,
-//            'text-anchor': 'left'
-//        });
-//
-//    d3.select("#update-list").on(
-//        "click",
-//        function () {
-//
-//            data.sort(function (a, b) {
-//                return b.academic - a.academic;
-//            });
-//
-//            console.log(data, function (d) {
-//                d.academic
-//            });
-//
-//            yScale.domain(data.map(function (d) {
-//                return d.schoolname;
-//            }));
-//
-//            svg.selectAll(".barLabel").transition().duration(1000).attr(
-//                "y", function (d) {
-//                    return yScale(d.schoolname) + 15;
-//                });
-//
-//            svg.selectAll(".bar").transition().duration(1000).attr("y",
-//                function (d) {
-//                    return yScale(d.schoolname);
-//                });
-
 
