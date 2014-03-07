72672868b212727956644138fe643e2651710098
Nils Gehlenborg <nils@gehlenborg.com>
2014-03-07 17:25:13 -0500

    Added events for attribute selection and de-selection.

--- items.js
+++ items.js
@@ -4,16 +4,20 @@ function plotSelectedItems() {
     
     area.html( "" );
 
-    var s = '<table><thead><tr><th>#</th><th>Id</th>';
+    var s = '';
+
+    s += '<p>Selection size: ' + selectedItems.length + ' of ' + depth + '</p>'
+
+    s += '<table><thead><tr><th>#</th><th>Id</th>';
 
     for ( var a = 0; a < attributes.length; ++a ) {
-        s += '<th>' + attributes[a].name + '</th>';
+        s += '<th class="attribute-header" data-attribute-id="' + a + '">' + attributes[a].name + '</th>';
     }
 
     area.append( '</tr></thead><tbody>' );
 
     for ( var i = 0; i < selectedItems.length; ++i ) {
-        s +=  '<tr id="' + ( 'item-' + i ) + '"><td><span class="item-index">' + (i+1) + '</span></td><td><span class="item-id">' + selectedItems[i] + '</span></td>';
+        s +=  '<tr class="item-row" data-item-id="' + selectedItems[i] + '"><td><span class="item-index">' + (i+1) + '</span></td><td><span class="item-id">' + selectedItems[i] + '</span></td>';
 
         for ( var a = 0; a < attributes.length; ++a ) {
             s += '<td><span class="item-label">' + attributes[a].values[selectedItems[i]] + '</span></td>';
@@ -25,4 +29,27 @@ function plotSelectedItems() {
     s += '</tbody></table>';
 
     area.append( s );
+
+    $( ".attribute-header" ).on( "click", function(){
+        if ( !selectedAttributes[this.getAttribute( 'data-attribute-id' )] ) {
+            selectedAttributes[this.getAttribute( 'data-attribute-id' )] = true;
+
+            $(EventManager).trigger( "attribute-selection-added", { id: this.getAttribute( 'data-attribute-id' ) } );
+        }
+        else {
+            delete selectedAttributes[this.getAttribute( 'data-attribute-id' )];   
+
+            $(EventManager).trigger( "attribute-selection-removed", { id: this.getAttribute( 'data-attribute-id' ) } );            
+        }
+
+        $(this).toggleClass( 'selected' );
+    });
 }
+
+$(EventManager).bind( "attribute-selection-added", function( event, data ) {
+    console.log( "attribute " + attributes[data.id].name + " was added to selection." );
+});
+
+$(EventManager).bind( "attribute-selection-removed", function( event, data ) {
+    console.log( "attribute " + attributes[data.id].name + " was removed from selection." );
+});
