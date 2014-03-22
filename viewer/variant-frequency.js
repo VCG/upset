8ce1eeaf316aee0c0113f970d50839e8ca3ad7b6
Nils Gehlenborg <nils@gehlenborg.com>
2014-03-22 13:34:23 -0400

    Added some comments.

--- viewer/variant-frequency.js
+++ viewer/variant-frequency.js
@@ -67,5 +67,62 @@ var variantFrequencyConfiguration = {
                 .append( 'p' )
                 .html( function( d ) {  return ( 'ti/tv = ' + ( d.transitions / d.transversions ) + ' (' + d.transitions + '/' + d.transversions + ')' ); } )
                 .style( "color", function( d ) { return ( d.color ); } );
+
+        /*
+        var margin = {top: 20, right: 20, bottom: 30, left: 40},
+            width = 960 - margin.left - margin.right,
+            height = 500 - margin.top - margin.bottom;
+
+        var x = d3.scale.ordinal()
+            .domain([0, 1, 2, 3])
+            .rangeBands([0, 1,2,3]);
+
+        var y = d3.scale.ordinal()
+            .domain([0, 1, 2, 3])
+            .rangeBands([0, 1,2,3]);
+
+        var z = d3.scale.ordinal()
+            .domain([0, 1, 2, 3])
+            .rangeBands([0, 1,2,3]);
+
+        var xAxis = d3.svg.axis()
+            .scale(x)
+            .orient("bottom");
+
+        var yAxis = d3.svg.axis()
+            .scale(y)
+            .orient("left")
+            .ticks(10, "%");
+
+        var svg = d3.select("body").append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+          .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+          svg.append("g")
+              .attr("class", "x axis")
+              .attr("transform", "translate(0," + height + ")")
+              .call(xAxis);
+
+          svg.append("g")
+              .attr("class", "y axis")
+              .call(yAxis)
+            .append("text")
+              .attr("transform", "rotate(-90)")
+              .attr("y", 6)
+              .attr("dy", ".71em")
+              .style("text-anchor", "end")
+              .text("Frequency");
+
+          svg.selectAll(".bar")
+              .data(data)
+            .enter().append("rect")
+              .attr("class", "bar")
+              .attr("x", function(d) { return x(d.letter); })
+              .attr("width", x.rangeBand())
+              .attr("y", function(d) { return y(d.frequency); })
+              .attr("height", function(d) { return height - y(d.frequency); });            
+        */
     }
 };
